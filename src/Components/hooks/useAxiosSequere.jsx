92440504c9f224e51e50ae9068453e8c0791ad09
Sabithasan2098 <sabithasan2098@gmail.com>
2023-11-18 18:55:50 +0600

    401,403 handle

--- src/Components/hooks/useAxiosSequere.jsx
+++ src/Components/hooks/useAxiosSequere.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiousSequere = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSequere = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
   axiousSequere.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
@@ -12,9 +17,26 @@ const useAxiosSequere = () => {
       return config;
     },
     function (error) {
+      console.log("status error in the interceptors", error);
       return Promise.reject(error);
     }
   );
+
+  axiousSequere.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error in the interceptors", status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiousSequere;
 };
 
