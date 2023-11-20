84fe0d1029b14e3f8869904258252283faa37d16
Sabithasan2098 <sabithasan2098@gmail.com>
2023-11-20 16:01:36 +0600

    add-item

--- src/Pages/Dashboard/AddIteams/AddIteams.jsx
+++ src/Pages/Dashboard/AddIteams/AddIteams.jsx
@@ -2,21 +2,45 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../Components/hooks/useAxiosPublic";
+import useAxiosSequere from "../../../Components/hooks/useAxiosSequere";
+import Swal from "sweetalert2";
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddIteams = () => {
   const { register, handleSubmit } = useForm();
   const axiosPublic = useAxiosPublic();
+  const axiosSequere = useAxiosSequere();
 
   const onSubmit = async (data) => {
     console.log(data);
     const imageFile = { image: data.image[0] };
     const res = await axiosPublic.post(image_hosting_api, imageFile, {
       headers: {
-        "content-type": "multipart/from-data",
+        "content-type": "multipart/form-data",
       },
     });
+    if (res.data.success) {
+      const menuItem = {
+        name: data.name,
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+        category: data.category,
+        price: data.price,
+      };
+      const menuRes = await axiosSequere.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.acknowledged) {
+        // get a popUp to complete
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "New menu item is added",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
     console.log(res.data);
   };
 
