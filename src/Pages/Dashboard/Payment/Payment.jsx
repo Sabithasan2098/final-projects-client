6afe0f98e2699f248cc3f294c1bfb585408b08a1
Sabithasan2098 <sabithasan2098@gmail.com>
2023-11-21 16:00:22 +0600

    payment

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,10 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheakoutForm from "./CheakoutForm";
+
+// TODO: add publisheable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_PK_Key);
 
 const Payment = () => {
   return (
@@ -7,6 +13,10 @@ const Payment = () => {
         heading={"pay before eat"}
         subHeading={"pay all of foods"}
       ></SectionTitle>
+
+      <Elements stripe={stripePromise}>
+        <CheakoutForm></CheakoutForm>
+      </Elements>
     </div>
   );
 };
